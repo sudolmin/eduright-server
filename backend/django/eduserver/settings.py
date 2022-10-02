06a3c53c116144b65c1717a14c0adb0a58bbf6d0
sudolmin <fireison20@protonmail.com>
2022-10-02 11:59:45 +0000

    eduserver #10

--- backend/django/eduserver/settings.py
+++ backend/django/eduserver/settings.py
@@ -4,9 +4,6 @@ import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open('/etc/secrets.json') as f:
-    secrets = json.load(f)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
