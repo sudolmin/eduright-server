e8f90e04fca22fd34a09e65feb2fd9de7c3c794e
sudolmin <fireison20@protonmail.com>
2022-10-02 12:32:36 +0000

    eduserver #12

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn -k uvicorn.workers.UvicornWorker --env DJANGO_SETTINGS_MODULE=eduserver.settings --bind 0.0.0.0:8080 eduserver.wsgi
+gunicorn -k uvicorn.workers.UvicornWorker --env DJANGO_SETTINGS_MODULE=eduserver.settings --bind 0.0.0.0:8080 eduserver.asgi
