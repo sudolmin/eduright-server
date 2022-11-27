923c71c06e282cc392881ec7689f1ac960b91ff9
sudolmin <fireison20@protonmail.com>
2022-11-27 16:42:06 +0000

    turning debug off

--- backend/django/eduserver/settings.py
+++ backend/django/eduserver/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 # allowed host
 ALLOWED_HOSTS = ["*"]
